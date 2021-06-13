859db58812fccf1b5b4dabc152f4f9cbfdaa12b6
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-06-12 22:21:01 -0400

    turn bfs into function + created dfs file

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,44 +1,49 @@
 from queue import Queue
 
-nodes_list = {
-    "A": ["D","F"],
+def bfs(graph):
+    
+    first_node = next(iter(graph))
+    
+    visited = {}
+    parent  = {}
+    level   = {} # distance 
+    result = []
+    queue = Queue()
+
+    for node in graph:
+        visited[node] = False
+        parent[node]  = None
+        level[node]   = -1
+
+    visited[first_node] = True
+    level[first_node] = 0
+    queue.put(first_node)
+    result = [first_node]
+
+    while not queue.empty():
+        curr_node = queue.get() # remove and return element
+        for child in graph[curr_node]:
+            if not visited[child]:
+                visited[child] = True
+                parent[child]  = curr_node
+                level[child]   = level[curr_node] + 1
+                queue.put(child)
+                result.append(child)
+                
+    return result
+
+
+graph = {
+    "A": ["B","D"],
     "B": ["A","C"],
-    "C": ["E","A"],
-    "D": ["C","F"],
-    "E": ["G","H"],
-    "F": ["A","F"],
-    "G": ["D","B"],
-    "H": ["A","F"],
+    "C": ["B"],
+    "D": ["A","E","F"],
+    "E": ["D","F","G"],
+    "F": ["D","E","H"],
+    "G": ["E","H"],
+    "H": ["G","F"],
 }
 
-visited = {}
-parent  = {}
-level   = {} # distance 
-
-result = []
-
-queue = Queue()
-
-
-for node in nodes_list:
-    visited[node] = False
-    parent[node]  = None
-    level[node]   = -1
-
-
-visited['A'] = True
-level['A'] = 0
-queue.put('A')
-result = ['A']
-
-while not queue.empty():
-    curr_node = queue.get() # remove and return element
-    for child in nodes_list[curr_node]:
-        if not visited[child]:
-            visited[child] = True
-            parent[child]  = curr_node
-            level[child]   = level[curr_node] + 1
-            queue.put(child)
-            result.append(child)
+result = bfs(graph)
 
 print(result)
