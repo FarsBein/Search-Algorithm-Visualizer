6a9d82adcc81676a35b25383b827b250a8cb481a
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-06-13 18:18:43 -0400

    depth first search function with example

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,3 +1,20 @@
+
+def DFS(graph):
+    stack = [next(iter(graph))]
+    visited = {}
+    results = []
+
+    while stack:
+        curr = stack[-1]
+        for node in nodes_list[curr]:
+            if node not in visited:
+                visited[node] = True
+                stack.append(node)
+                break
+            if nodes_list[curr][-1] == node:
+                results.append(stack.pop())
+    return results
+
 nodes_list = {
     "A": ["B","D"],
     "B": ["A","C"],
@@ -7,6 +24,10 @@ nodes_list = {
     "F": ["D","E","H"],
     "G": ["E","H"],
     "H": ["G","F"],
-}
+} 
 
-stack = []
+results = DFS(nodes_list)
+        
+results.pop()
+            
+print(results)
